e90b49c6ee53c13710f95bd483edf4b2a281daea
sohansatpute <satputesohan8@gmail.com>
2023-05-03 07:46:52 +0530

    dev-3

--- Script1.sh
+++ Script1.sh
@@ -2,16 +2,4 @@
 #This is my 1st shell script to print output
 echo "Hello All, Welcome to AWS/Devops Class"
 echo "Hello, How are you?"
-echo "Welcome to Young Minds"
-echo "Best class of AWS/DevOps"
-echo "Hello Team"
-echo "My name is Khan"
-echo "Welcome to devops"
-echo "We are learning git"
-echo "This batch-18"
-echo "Good morning"
 
-echo "Welcome to devops"
-echo "We are learning git"
-echo "This batch-18"
-echo "Good morning"
