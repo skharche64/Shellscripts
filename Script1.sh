034e63c1f7d36117a931769dfb91d4eff04cb5cc
sohansatpute <satputesohan8@gmail.com>
2024-01-24 08:31:49 +0530

    Revert the changes

--- Script1.sh
+++ Script1.sh
@@ -5,5 +5,3 @@ echo "Hello, How are you?"
 echo "Welcome to Young Minds"
 echo "Hello batch-21, We are learning Jenkins"
 echo "Good morning batch-22, We are learning GitHub"
-echo "We are learning branching strategies"
-
